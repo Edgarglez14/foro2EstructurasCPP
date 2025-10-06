de4bc185fe13db61abd025615e716e1f73e073b5
Edgar Gonzalez <zS25018124@estudiantes.uv.mx>
2025-10-06 09:36:07 -0600

    programa if else

--- programa1ifelse.cpp
+++ programa1ifelse.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main() {
+int opcion;
+
+do
+{
+  cout << "==Menu==" << endl;
+  cout << "1.-Torta" << endl;
+  cout << "2.-palomitas" << endl;
+  cout << "3.-agua" << endl;
+  cout << "4.-salir" << endl;
+
+  cin >> opcion;
+if (opcion == 1){
+   cout << "torta: 45 pesos" << endl;
+
+} else if (opcion ==2){
+    cout << "palomitas: 20 pesos" << endl;
+}
+ else if (opcion ==3)
+ {
+    cout << "palomitas: 20 pesos" << endl;
+ }
+ 
 
 
 
@@ -11,6 +33,18 @@ int main() {
 
 
 
-    
+} while (opcion !=4);
+
+   
+return 0;
+
+
+
+
+
+
+
+
+
 }
 
